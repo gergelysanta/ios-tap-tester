35710a376b5f1060f99de3121d1f73f6e0867990
Gergely Sánta <gergely.santa@trikatz.com>
2017-11-13 19:16:33 +0100

    Some minor refactoring

--- TapTester/ViewController.swift
+++ TapTester/ViewController.swift
@@ -12,22 +12,31 @@ import CoreGraphics
 
 class ViewController: UIViewController {
 	
-	typealias TouchPoint = (start: CGPoint, last: CGPoint)
+	typealias TouchMove = (start: CGPoint, last: CGPoint)
 	
+	// SpriteKit scene displaying touch visualisations
 	let scene = SKScene()
-	var touchHistory = [UITouch:TouchPoint]()
 	
+	// Dictionary holding the first and last location for each active touch
+	var touchHistory = [UITouch:TouchMove]()
+	
+	// Duration of animations
 	let actionChangeInterval:CGFloat = 0.7
 	
-	let lineStartColor = UIColor.red
-	let lineEndColor = UIColor.lightGray
-	
+	// Starting and ending size of touch lines (touch movement visualisation)
 	let lineStartSize:CGFloat = 10.0
 	let lineEndSize:CGFloat = 2.0
 	
-	let pointDotSize:CGFloat = 50.0
-	let pointEndSize:CGFloat = 8.0
+	// Starting and ending size of tap dots (tap visualisation)
+	let tapDotStartSize:CGFloat = 50.0
+	let tapDotEndSize:CGFloat = 8.0
 
+	// Starting and ending color of touch lines / tap dots
+	let touchStartColor = UIColor.red
+	let touchEndColor = UIColor.lightGray
+	
+	// MARK: - Initialization
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		if let skView = view as? SKView {
@@ -38,14 +47,16 @@ class ViewController: UIViewController {
 		scene.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
 	}
 	
-	func drawLine(from: CGPoint, to: CGPoint, startWidth: CGFloat, endWidth: CGFloat) {
+	// MARK: - Private methods
+	
+	private func drawLine(from: CGPoint, to: CGPoint, startWidth: CGFloat, endWidth: CGFloat) {
 		let line = SKShapeNode()
 		let path = CGMutablePath()
 		path.move(to: from)
 		path.addLine(to: to)
 		line.path = path
 		line.lineCap = .round
-		line.strokeColor = lineEndColor
+		line.strokeColor = touchEndColor
 		line.lineWidth = startWidth
 		
 		let action = SKAction.customAction(withDuration: TimeInterval(actionChangeInterval)) { (node: SKNode, elapsedTime: CGFloat) in
@@ -53,8 +64,8 @@ class ViewController: UIViewController {
 			
 			var startRed:CGFloat = 0, startGreen:CGFloat = 0, startBlue:CGFloat = 0, startAlpha:CGFloat = 0
 			var endRed:CGFloat = 0, endGreen:CGFloat = 0, endBlue:CGFloat = 0, endAlpha:CGFloat = 0
-			self.lineStartColor.getRed(&startRed, green: &startGreen, blue: &startBlue, alpha: &startAlpha)
-			self.lineEndColor.getRed(&endRed, green: &endGreen, blue: &endBlue, alpha: &endAlpha)
+			self.touchStartColor.getRed(&startRed, green: &startGreen, blue: &startBlue, alpha: &startAlpha)
+			self.touchEndColor.getRed(&endRed, green: &endGreen, blue: &endBlue, alpha: &endAlpha)
 			
 			if let line = node as? SKShapeNode {
 				line.lineWidth = startWidth + (endWidth - startWidth) * step
@@ -100,7 +111,7 @@ class ViewController: UIViewController {
 			if let oldLocation = touchHistory[touch] {
 				let distance = hypot(location.x - oldLocation.start.x, location.y - oldLocation.start.y)
 				if distance < lineStartSize {
-					drawLine(from: oldLocation.start, to: location, startWidth: pointDotSize, endWidth: pointEndSize)
+					drawLine(from: oldLocation.start, to: location, startWidth: tapDotStartSize, endWidth: tapDotEndSize)
 				}
 			}
 			touchHistory.removeValue(forKey: touch)
